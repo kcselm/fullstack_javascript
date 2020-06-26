d403413e2eac7ac4d5a1349aab89a7d1a1655408
kcselm <kcselm93@gmail.com>
2020-06-26 14:06:06 -0500

    Countries step 3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,8 +5,11 @@ import axios from 'axios'
 
 const App = () => {
 
+  const api_key = process.env.REACT_APP_API_KEY
   const [searchTerm, setSearchTerm] = useState('')
   const [countries, setCountries] = useState([])
+  const [capital, setCapital] = useState('')
+  const [weather, setWeather] = useState({})
 
   const handleSearchTerm = e => {
     e.preventDefault()
@@ -15,39 +18,63 @@ const App = () => {
 
   const handleShowView = country => {
     // setSearchTerm(country)
-    console.log(country)
+    // console.log(country)
     setCountries([country])
+    // console.log('capital', country.capital)
+    setCapital(country.capital)
   }
 
   useEffect(() => {
     axios
       .get(`https://restcountries.eu/rest/v2/name/${searchTerm}`)
       .then(response => {
-        console.log(response.data)
+        console.log('country data', response.data)
         setCountries(response.data)
       })
       .catch(error => console.log(error))
   }, [searchTerm])
 
-  const countryView = countries.map(country => 
-    <div key={country.name}>
-      <h3>{country.name}</h3>
-      <ul>capital: {country.capital} </ul>
-      <ul>population: {country.population}</ul>
-      <h3>Languages</h3>
-      {country.languages.map(language => 
-        <li key={language.name}>{language.name}</li>
-      )}
-      <img src={country.flag}></img>
-    </div>
-  )
+  useEffect(() => {
+    let url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`
+    console.log('url', url)
+    axios
+      .get(url)
+      .then(response => {
+        console.log('weather data', response.data.current)
+        setWeather(response.data.current)
+        console.log('weather', weather)
+      })
+      .catch(error => console.log(error))
+  }, [capital])
 
+  const countryView = () => {
+    if (weather) {
+      return (
+        countries.map(country => 
+        <div key={country.name}>
+          <h3>{country.name}</h3>
+          <ul>capital: {country.capital} </ul>
+          <ul>population: {country.population}</ul>
+          <h3>Languages</h3>
+          {country.languages.map(language => 
+            <li key={language.name}>{language.name}</li>
+          )}
+          <img src={country.flag}></img>
+          <h3>Weather in {capital}</h3>
+          <p><strong>teperature: </strong>{weather.temperature} Celcius </p>
+          <img src={weather.weather_icons}></img>
+          <p><strong>wind: </strong>{weather.wind_speed} mph direction {weather.wind_dir}</p>
+        </div>
+        )
+      )
+    }
+  }
 
   const controlOutput = countries => {
     if (countries.length >= 10) {
       return <p>Too many matches, specify another filter</p> 
     } else if (countries.length === 1) {
-      return countryView
+      return countryView()
     } else {
       return (
         countries.map(country => 
