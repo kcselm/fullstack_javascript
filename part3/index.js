6cdc4190b0f2a2037856bed9567e5936880f66c2
kcselm <kcselm93@gmail.com>
2020-09-23 19:02:34 -0500

    Phonebook backend step 5

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 let persons = [
   {
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+//GET all persons
 app.get('/api/persons', (req, res) => {
   const numberOfPeople = persons.map(person => person.name).length
   const time = Date.now()
@@ -41,25 +43,50 @@ app.get('/api/persons', (req, res) => {
   )
 })
 
-
+//GET a particular person
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
   if (person) {
     res.json(person)
   } else {
-    res.send(
-      '<p>There are no people with that id</p>'
-    )
+    // response.status(404).end()
+    res.send('<p>There are no people with that id</p>')
   }
   })
 
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    res.status(204).end()
-  })
+//POST a new person to the object
+app.post('/api/persons', (req, res) => {
+  // create id's between 100 and 1000
+  const id = 100 + Math.floor(Math.random() * 900) 
+  // const person = {
+  //   name: "",
+  //   number: "",
+  //   id: id
+  // }
+  // res.send(`<h1>${id}</h1>`) 
+
+  const person = req.body
+  console.log(person)
+
+  res.json(person)
+
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+
+  response.json(person)
+})
+
+//DELETE person from the object
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
 
 
 const PORT = 3001
